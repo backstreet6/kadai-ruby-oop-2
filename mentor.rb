e2452586383fe003725f5ce2fe4d0fd24dc2ec57
satoshi.fujita <not_rishi@yahoo.co.jp>
2021-09-21 06:06:34 +0000

    kadai-ruby-oop-2

---
+++ mentor.rb
@@ -0,0 +1,25 @@
+class Mentor
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+ 
+class RailsMentor < Mentor
+  def job
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+end
+  
+# インスタンスの生成と、変数への代入
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
+
+# インスタンスの使用
+kirameki.job
+akaide.job
